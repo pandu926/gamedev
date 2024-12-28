c759b0bd621cc1903a9093c8ad741176c3298e6d
pandu926 <ahmadpandusubekti1@gmail.com>
2024-12-28 18:10:17 +0700

    ya

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1735383174|8842960328';
+const CACHE_VERSION = '1735384199|9867815174';
 /** @type {string} */
 const CACHE_PREFIX = 'New Game Project-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
